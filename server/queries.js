64960f7c454d0c7178efac6accd21263d8e9c6bc
nomanoma121 <takeuchi180121@gmail.com>
2024-10-20 02:41:09 +0900

    fix: クエリを持つオブジェクトだけを定義

--- server/queries.js
+++ server/queries.js
@@ -1,31 +1,28 @@
-import Database from "better-sqlite3";
+const Users = {
+  createTable: `
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      weight REAL NOT NULL,
+      email TEXT NOT NULL,
+      password TEXT NOT NULL
+    );`
+};
 
-// 仮でdatabase.dbとしている
-const db = new Database("database.db");
+const Drinking_Alcohol = {
+  createTable: `
+    CREATE TABLE IF NOT EXISTS drinking_records (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user_id INTEGER,
+      alcohol_amount REAL NOT NULL,
+      condition INTEGER NOT NULL,
+      date INTEGER NOT NULL,
+      updated_at INTEGER NOT NULL,
+      FOREIGN KEY (user_id) REFERENCES users(id)
+    );`
+};
 
-const createUsersTableQuery = db.prepare(`
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT NOT NULL,
-  weight REAL NOT NULL,
-  email TEXT NOT NULL,
-  password TEXT NOT NULL
-);
-`);
-
-const createDrinkingRecordsTableQuery = db.prepare(`
-CREATE TABLE IF NOT EXISTS drinking_records (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER,
-  alcohol_amount REAL NOT NULL,
-  condition INTEGER NOT NULL,
-  date INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
-`);
-
-createUsersTableQuery.run();
-createDrinkingRecordsTableQuery.run();
-
-export default db;
+module.exports = {
+  Users,
+  Drinking_Alcohol,
+};
