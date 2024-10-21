920916d509ed1ed0a9e4a5f08c8378135a36543e
kou <107801494+nomanoma121@users.noreply.github.com>
2024-10-21 23:42:09 +0900

    feat: パスワードのハッシュ化を実装

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import Database from "better-sqlite3";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import bcrypt, { hash } from "bcrypt";
 import queries from "./queries.js";
 
 const app = new Hono();
@@ -15,20 +16,24 @@ app.get("/api/hello", (c) => {
   return c.json({ message: "Hello, Alcoho-Rhythm server!" });
 });
 
-
 app.post("/api/signup", async (c) => {
   const param = await c.req.json();
+  const hashPassword = async (password) => {
+    const hash = await bcrypt.hash(password, 12);
+    return hash;
+  }
+
+  const hashedPassword = await hashPassword(param.password);
 
   db.prepare(queries.Users.create).run(
     param.username,
     param.weight,
     param.email,
-    param.password
+    hashedPassword,
   );
 
-  return c.json({ message: "Successfully created!" });
+  return c.json({ message: "Successfully created." });
 });
-
 
 migrate(db);
 
