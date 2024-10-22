01890d62748aa22b0ab7e4667a3296eb35c6885b
shigek <mizokyuu114@gmail.com>
2024-10-22 10:57:14 +0900

    fix:cssを整理

--- client/src/register/Register.jsx
+++ client/src/register/Register.jsx
@@ -12,34 +12,91 @@ function Register() {
 
       {/* 酒の数量登録 */}
       <div className="alcohol-register">
-        <h2>アルコール数量登録</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>お酒の種類</th>
-              <th>数量</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>ビール</td>
-              <td><input type="number" placeholder="数量を入力" name="beer" /></td>
-            </tr>
-            <tr>
-              <td>ハイボール</td>
-              <td><input type="number" placeholder="数量を入力" name="highball" /></td>
-            </tr>
-            <tr>
-              <td>日本酒</td>
-              <td><input type="number" placeholder="数量を入力" name="sake" /></td>
-            </tr>
-            <tr>
-              <td>焼酎</td>
-              <td><input type="number" placeholder="数量を入力" name="shochu" /></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <h2>アルコール数量登録</h2>
+      <table className="alcohol-table">
+        <thead>
+          <tr>
+            <th>お酒</th>
+            <th>アルコール度数</th>
+            <th>数量</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              ビール・発泡酒・チューハイなど
+              <br />
+              <small>※1缶＝350mlとして</small>
+            </td>
+            <td>5%</td>
+            <td><input type="number" min="0" /> 缶</td>
+          </tr>
+          <tr>
+            <td>
+              ビール・発泡酒・チューハイなど
+              <br />
+              <small>※1缶＝500mlとして</small>
+            </td>
+            <td>5%</td>
+            <td><input type="number" min="0" /> 缶</td>
+          </tr>
+          <tr>
+            <td>
+              ハイボールなど
+              <br />
+              <small>※1缶＝350mlとして</small>
+            </td>
+            <td>7%</td>
+            <td><input type="number" min="0" /> 缶</td>
+          </tr>
+          <tr>
+            <td>
+              ハイボールなど
+              <br />
+              <small>※1缶＝500mlとして</small>
+            </td>
+            <td>7%</td>
+            <td><input type="number" min="0" /> 缶</td>
+          </tr>
+          <tr>
+            <td>
+              ワインなど
+              <br />
+              <small>※1杯＝120mlとして</small>
+            </td>
+            <td>12%</td>
+            <td><input type="number" min="0" /> 杯</td>
+          </tr>
+          <tr>
+            <td>
+              清酒など
+              <br />
+              <small>※1合＝180mlとして</small>
+            </td>
+            <td>15%</td>
+            <td><input type="number" min="0" /> 合</td>
+          </tr>
+          <tr>
+            <td>
+              焼酎など
+              <br />
+              <small>※コップ1杯＝100mlとして</small>
+            </td>
+            <td>25%</td>
+            <td><input type="number" min="0" /> 杯</td>
+          </tr>
+          <tr>
+            <td>
+              ウイスキー、ブランデーなど
+              <br />
+              <small>※シングル1杯＝30mlとして</small>
+            </td>
+            <td>40%</td>
+            <td><input type="number" min="0" /> 杯</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
       {/* 体調の登録 */}
       <div className="condition-register">
