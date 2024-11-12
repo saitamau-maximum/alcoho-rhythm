20fe3f73b04e49f8ca6ccfd3dc41953b8832b812
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-11-13 00:47:12 +0900

    fix: drinking_recordsのupdateクエリにdateの項目を追加

--- server/queries.js
+++ server/queries.js
@@ -25,7 +25,7 @@ const DrinkingRecords = {
   create: `INSERT INTO drinking_records (user_id, alcohol_amount, condition, date, updated_at) VALUES (?, ?, ?, strftime('%s', ?), strftime('%s', ?))`,
   findById: `SELECT * FROM drinking_records WHERE id = ?`,
   findByUserIdAndDateRange: `SELECT * FROM drinking_records WHERE user_id = ? AND date BETWEEN strftime('%s', ?) AND strftime('%s', ?)`,
-  update: `UPDATE drinking_records SET alcohol_amount = ?, condition = ?, updated_at = strftime('%s', ?) WHERE id = ?`,
+  update: `UPDATE drinking_records SET alcohol_amount = ?, condition = ?, date = strftime('%s', ?), updated_at = strftime('%s', ?) WHERE id = ?`,
   delete: `DELETE FROM drinking_records WHERE id = ?`,
 };
 
