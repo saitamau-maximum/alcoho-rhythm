930503dd4304ae4fdc1bd04e5684d1956f1e5a8b
shigek <mizokyuu114@gmail.com>
2024-11-27 03:06:24 +0900

    fix: signへのリダイレクト

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Signin from "./signin/Signin";
 import Register from "./register/Register";
 import Signup from "./signup/Signup";
+
+
+function isAuthenticated() {
+  return document.cookie.split(';').some((item) => item.trim().startsWith('token='));
+}
+
+function PrivateRoute({ children }) {
+  return isAuthenticated() ? children : <Navigate to="/signin" replace />;
+}
 
 function App() {
   return (
@@ -11,10 +20,24 @@ function App() {
       <BrowserRouter>
         <Link to="/signup">Signup</Link> | <Link to="/signin">Signin</Link> |{" "}
         <Link to="/dashboard">Dashboard</Link> |{" "}
-        <Link to="/Register">Register</Link>
+        <Link to="/register">Register</Link>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/register" element={<Register />} />
