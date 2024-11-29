7600d65dab748d69ecca306da6bc512399ff14b8
shigek <mizokyuu114@gmail.com>
2024-11-29 22:41:59 +0900

    fix: lint6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes, Link, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Signin from "./signin/Signin";
 import Register from "./register/Register";
-import Signup from "./signup/Signup"
+import Signup from "./signup/Signup";
 
 // 認証状態を管理
 function useAuth() {
