3271a461f251126adf0413eedecf3a61c38a61f1
kouta <takeuchi180121@gmail.com>
2024-12-01 01:43:20 +0900

    feat: fetchをDashboard.jsxでまとめて実行するようにした

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,9 +1,33 @@
-import  DrinkingAmountGraph  from "./DrinkingAmountGraph";
+import { useEffect, useState } from "react";
+import DrinkingAmountGraph from "./DrinkingAmountGraph";
 
 function Dashboard() {
+  const [fetchedData, setFetchedData] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8000/api/recordsTest", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        start: "2024-11-01T00:00:00Z",
+        end: "2024-11-31T23:59:59Z",
+      }),
+      credentials: "include", // Cookieを送信
+    });
+    const data = await response.json();
+    //fetchしたデータを更新
+    setFetchedData(data);
+  };
+
+  //マウント時に実行
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <DrinkingAmountGraph />
+      <DrinkingAmountGraph fetchedData={fetchedData}/>
     </div>
   );
 }
--- client/src/DrinkingAmountGraph.jsx
+++ client/src/DrinkingAmountGraph.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { countDrinkingAmount } from './countDrinkingAmount';
-import { Bar } from 'react-chartjs-2';
+import React, { useState, useEffect } from "react";
+import { countDrinkingAmount } from "./countDrinkingAmount";
+import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,41 +9,31 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from "chart.js";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
-function Dashboard() {
+function DrinkingAmountGraph({ fetchedData }) {
   const [data, setData] = useState({
     labels: [],
     datasets: [
       {
-        label: '飲酒量',
+        label: "飲酒量",
         data: [],
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
         borderWidth: 1,
       },
     ],
   });
 
-  // APIからデータを取得して、飲酒量を集計
-  const fetchData = async () => {
-    const response = await fetch('http://localhost:8000/api/recordsTest', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        start: '2024-11-01T00:00:00Z',
-        end: '2024-11-31T23:59:59Z',
-      }),
-      credentials: 'include', // Cookieを送信
-    });
-
-    const fetchedData = await response.json();
-    console.log(fetchedData);
-
-    // 日付ごとの飲酒量を取得
+  useEffect(() => {
     const drinkingAmountPerDay = countDrinkingAmount(fetchedData);
 
     console.log(drinkingAmountPerDay);
@@ -56,25 +46,21 @@ function Dashboard() {
       labels: labels,
       datasets: [
         {
-          label: '飲酒量',
+          label: "飲酒量",
           data: drinkingAmountPerDay,
-          backgroundColor: 'rgba(98, 193, 227, 1)',
+          backgroundColor: "rgba(98, 193, 227, 1)",
           borderWidth: 1,
         },
       ],
     });
-  };
-  //マウント時にデータを取得
-  useEffect(() => {
-    fetchData();
-  }, []);
+  }, [fetchedData]);
 
   const options = {
     responsive: true,
     plugins: {
       title: {
         display: true,
-        text: '飲酒量データ',
+        text: "飲酒量データ",
       },
     },
   };
@@ -86,4 +72,4 @@ function Dashboard() {
   );
 }
 
-export default Dashboard;
+export default DrinkingAmountGraph;
