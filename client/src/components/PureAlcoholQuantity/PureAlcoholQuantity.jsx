4718c6e41e494cb4ea743a09e1b2ff7892bdec6a
hana-rs <hanahanabi87@icloud.com>
2024-12-02 16:57:52 +0900

    feat:体調ごとの純アルコール量のカードを作った

--- client/src/components/PureAlcoholQuantity/PureAlcoholQuantity.jsx
+++ client/src/components/PureAlcoholQuantity/PureAlcoholQuantity.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { calcPureAlcoholQuantityAvg } from "../../utils/calcPureAlcoholQuantityAvg";
 import PropTypes from "prop-types";
+import "./PureAlcoholQuantity.css";
 
 PureAlcoholQuantity.propTypes = {
   fetchedData: PropTypes.arrayOf(
@@ -17,9 +18,9 @@ function PureAlcoholQuantity({ fetchedData }) {
   }, [fetchedData]);
   console.log(pureAlcoholQuantity);
   return (
-    <div>
+    <div className="pure-alcohol-quantity-container">
       <h3>純アルコール量</h3>
-      <div>
+      <div className="pure-alcohol-quantity-2">
         {pureAlcoholQuantity.map((item, index) => (
           <span key={index} style={{ margin: "20px" }}>
             {item}
@@ -28,7 +29,7 @@ function PureAlcoholQuantity({ fetchedData }) {
         {/* この下は一時的に見やすいようにしてるだけ */}
         <span style={{ display: "block" }}></span>
         <span>悪い</span>
-        <span>←-------------------------→</span>
+        <span className="yajirushi">←-------------------------→</span>
         <span>良い</span>
       </div>
     </div>
